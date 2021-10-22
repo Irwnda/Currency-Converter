ccf1ee65bde3fc72722a0e75fe0940c594944bed
Akbar Irwanda <irwandaa7@gmail.com>
2021-10-22 22:42:35 +0700

    update rate.json location
    Agar bisa disimpan dalam folder public.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import CurrencyRow from "./components/CurrencyRow/CurrencyRow";
 import ExchangeResult from "./components/ExchangeResult/ExchangeResult";
-import rawData from "./components/data/rate.json";
-let rates = rawData[0].rates;
+// import rawData from "./components/data/rate.json";
+// let rates = rawData[0].rates;
 
 function App() {
   const [fromCurrency, setFromCurrency] = useState();
   const [toCurrency, setToCurrency] = useState();
-  const [currencyOptions, setCurrencyOptions] = useState([]);
   const [currencyValue, setCurrencyValue] = useState(1);
+  const [rates, setRates] = useState([]);
+
+  useEffect(() => {
+    fetch("data/rate.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myData) {
+        setRates(myData[0].rates);
+      });
+    setFromCurrency("USD");
+    setToCurrency("IDR");
+  }, []);
+  console.log(Object.keys(rates));
 
   let codes = [];
   for (let code in rates) {
@@ -20,12 +38,6 @@ function App() {
     );
   }
 
-  useEffect(() => {
-    setCurrencyOptions([...Object.keys(rates)]);
-    setFromCurrency("USD");
-    setToCurrency("IDR");
-  }, []);
-
   return (
     <>
       <header>Currency Converter</header>
@@ -35,14 +47,14 @@ function App() {
           <input
             type="number"
             value={currencyValue}
-            onChange={(e) => setCurrencyValue(e.target.value)}
+            onChange={(e) => setCurrencyValue(Math.abs(e.target.value))}
           />
         </div>
         <div className="drop-list">
           <div className="From">
             <p>From</p>
             <CurrencyRow
-              currencyOptions={currencyOptions}
+              currencyOptions={Object.keys(rates)}
               selectedCurrency={fromCurrency}
               onChangeCurrency={(e) => setFromCurrency(e.target.value)}
             />
@@ -60,7 +72,7 @@ function App() {
           <div className="To">
             <p>To</p>
             <CurrencyRow
-              currencyOptions={currencyOptions}
+              currencyOptions={Object.keys(rates)}
               selectedCurrency={toCurrency}
               onChangeCurrency={(e) => setToCurrency(e.target.value)}
             />
